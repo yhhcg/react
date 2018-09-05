8370744600207aa5d52a20663650c0bdfe90dad1
余珩 <1281005689@qq.com>
2018-09-05 22:10:54 +0800

    [core] Use <Provider> to magically make the store available to all container component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux'; 
 
+import rootReducer from './reducers'
 import Router from './router';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <Router />,
+  <Provider store={store}>
+    <Router />
+  </Provider>,
   document.getElementById('app')
 );
---
+++ src/reducers.js
@@ -0,0 +1,12 @@
+import {combineReducers} from 'redux';
+
+const app = (state={}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  app,
+});
