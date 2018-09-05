7bc0a3e97588224b7628bd679b702c85b7b32f8e
余珩 <1281005689@qq.com>
2018-09-05 22:26:57 +0800

    [core] Add babel-preset-stage-0 to convert the spread operator of ECMAScript 2015+ code

--- src/reducers.js
+++ src/reducers.js
@@ -2,6 +2,11 @@ import {combineReducers} from 'redux';
 
 const app = (state={}, action) => {
   switch (action.type) {
+    case 'GET_USER':
+      return {
+        ...state,
+        user: 'yuheng',
+      };
     default:
       return state;
   }
