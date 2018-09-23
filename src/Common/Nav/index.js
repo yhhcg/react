475c6ed26ebfdd1f1997d524625a0a2b0b12d5b1
余珩 <1281005689@qq.com>
2018-09-23 19:50:02 +0800

    [Nav] Run typechecking on the props

--- src/Common/Nav/index.js
+++ src/Common/Nav/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {
+  arrayOf,
+  shape,
+  string,
+} from 'prop-types';
 
 import classes from './index.less';
 
 class Nav extends React.Component {
+  static propTypes = {
+    data: arrayOf(shape({
+      id: string.isRequired,
+      name: string.isRequired,
+    })),
+  };
+
   static defaultProps = {
     data: [],
   };
