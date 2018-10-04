6e4d06de528a16bd7cac70a2264ac797dcfcfca0
余珩 <1281005689@qq.com>
2018-10-04 16:38:56 +0800

    [Login] Record userName and password with state

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -4,13 +4,46 @@ import TextField from 'Common/TextField';
 import classes from './index.less';
 
 class Login extends React.Component {
+  state = {
+    password: '',
+    userName: '',
+  };
+
+  handleClick = (event) => {
+    console.log(this.state);
+  }
+
+  handlePasswordChange = (event) => {
+    this.setState({
+      ...this.state,
+      password: event.target.value,
+    });
+  }
+
+  handleUsernameChange = (event) => {
+    this.setState({
+      ...this.state,
+      userName: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div className={classes.root}>
         <div className={classes.loginBox}>
-          <TextField label="Username" />
-          <TextField label="Password" type="password"/>
-          <Button name="登 录" />
+          <TextField
+            label="Username"
+            onChange={this.handleUsernameChange}
+          />
+          <TextField
+            label="Password"
+            onChange={this.handlePasswordChange}
+            type="password"
+          />
+          <Button
+            name="登 录"
+            onClick={this.handleClick}
+          />
         </div>
       </div>
     );
