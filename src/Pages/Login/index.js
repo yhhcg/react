d2255bd2658ce5fd978a13dfdfce3e999dc56516
余珩 <1281005689@qq.com>
2018-10-04 15:17:01 +0800

    [Login] Rewrite login page with textField and button component

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from 'Common/Button';
+import TextField from 'Common/TextField';
 import classes from './index.less';
 
 class Login extends React.Component {
@@ -6,18 +8,13 @@ class Login extends React.Component {
     return (
       <div className={classes.root}>
         <div className={classes.loginBox}>
-          <label>
-            <div>Username</div>
-            <input type="text" />
-          </label>
-          <label>
-            <div>Password</div>
-            <input type="password"/>
-          </label>
+          <TextField label="Username" />
+          <TextField label="Password" type="password"/>
+          <Button name="登录" />
         </div>
       </div>
     );
-  }
+  } 
 }
 
 export default Login;
