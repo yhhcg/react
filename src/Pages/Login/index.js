e172365f7d3da5ab4cf93e8a7c2e24735cfcf2b1
余珩 <1281005689@qq.com>
2018-10-04 14:32:54 +0800

    [Login] Render login box to adapt to the  mobile

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import classes from './index.less';
 
 class Login extends React.Component {
   render() {
-    return <div>Login page</div>;
+    return (
+      <div className={classes.root}>
+        <div className={classes.loginBox}>
+          <label>
+            <div>Username</div>
+            <input type="text" />
+          </label>
+          <label>
+            <div>Password</div>
+            <input type="password"/>
+          </label>
+        </div>
+      </div>
+    );
   }
 }
 
