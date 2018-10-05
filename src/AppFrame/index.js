b87a12dd8f74ed3d0c38350336f2fe4d40ce8981
余珩 <1281005689@qq.com>
2018-10-05 14:18:25 +0800

    [AppFrame] Pass updated match, location, and history props to the appFrame component via the withRouter higher-order component and redirect to page once the nav is clicked

--- src/AppFrame/index.js
+++ src/AppFrame/index.js
@@ -2,13 +2,17 @@ import {
   default as React,
   Fragment,
 } from 'react';
-
+import {withRouter} from 'react-router'
 import CssBaseline from 'Common/CssBaseline';
 import Nav from 'Common/Nav';
 import classes from './index.less';
 import logoIcon from './logo.png';
 
 class AppFrame extends React.Component {
+  state = {
+    selectedId: '',
+  };
+
   nav = [{
     id: 'home',
     name: '首页',
@@ -19,12 +23,37 @@ class AppFrame extends React.Component {
 
   constructor(props) {
     super(props);
+
+    const {
+      location,
+    } = this.props;
+
+    this.state = {
+      selectedId: location.pathname.match(/^\/(.+)$/)[1],
+    };
   }
+
+  handleNavClick = ({id}) => {
+    const {
+      history,
+    } = this.props;
+
+    this.setState({
+      ...this.state,
+      selectedId: id,
+    });
+
+    history.push(`/${id}`);
+  };
 
   render() {
     const {
       children,
     } = this.props;
+
+    const {
+      selectedId,
+    } = this.state;
 
     return (
       <Fragment>
@@ -32,7 +61,11 @@ class AppFrame extends React.Component {
         <div className={classes.root}>
           <div className={classes.header}>
             <img className={classes.logo} src={logoIcon} />
-            <Nav data={this.nav} />
+            <Nav
+              data={this.nav}
+              onClick={this.handleNavClick}
+              selectedId={selectedId}
+            />
           </div>
           <div>
             {children}
@@ -43,4 +76,4 @@ class AppFrame extends React.Component {
   }
 }
 
-export default AppFrame;
+export default withRouter(AppFrame);
