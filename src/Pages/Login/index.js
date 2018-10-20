94f0f4099943620cae86495f08f594a29cfbf055
余珩 <1281005689@qq.com>
2018-10-20 11:15:15 +0800

    [Login] Rewrite login page that provide home page entry

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,49 +1,12 @@
 import React from 'react';
-import Button from 'Common/Button';
-import TextField from 'Common/TextField';
-import classes from './index.less';
 
 class Login extends React.Component {
-  state = {
-    password: '',
-    userName: '',
-  };
-
-  handleClick = (event) => {
-    console.log(this.state);
-  }
-
-  handlePasswordChange = (event) => {
-    this.setState({
-      ...this.state,
-      password: event.target.value,
-    });
-  }
-
-  handleUsernameChange = (event) => {
-    this.setState({
-      ...this.state,
-      userName: event.target.value,
-    });
-  }
-
   render() {
     return (
-      <div className={classes.root}>
-        <div className={classes.loginBox}>
-          <TextField
-            label="Username"
-            onChange={this.handleUsernameChange}
-          />
-          <TextField
-            label="Password"
-            onChange={this.handlePasswordChange}
-            type="password"
-          />
-          <Button
-            name="登 录"
-            onClick={this.handleClick}
-          />
+      <div>
+        Login page
+        <div>
+          <a href="/home">redirect to home page</a>
         </div>
       </div>
     );
